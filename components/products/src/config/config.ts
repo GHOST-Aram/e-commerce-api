485013169579bbe3b3a8146edbaa5756400f71d8
ghost-aramic <owinoojala@gmail.com>
2023-11-22 12:08:30 +0300

    feat: Configure express app
         - Install express, morgan, cors, helmet
         - Create config file
         - Make initial express app configurations

---
+++ components/products/src/config/config.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express"
+import cors from "cors"
+import helmet from "helmet"
+import logger from "morgan"
+
+const app: Application = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(logger('dev'))
+
+
+export { app }
