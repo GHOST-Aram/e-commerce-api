738febdeb4c3a25f8f22df2d266ef736156dd4d6
ghost-aramic <owinoojala@gmail.com>
2023-11-22 20:50:23 +0300

    feat: Create custom string formatter
         - Create Formmater class
         - Create formatFieldName method to be used in validators

---
+++ components/products/src/utils/formatter.test.ts
@@ -0,0 +1,14 @@
+import { test, expect, describe } from "@jest/globals";
+import { formatter } from "./formatter";
+
+describe('Formatter Class', () =>{
+    test('Capitalizes first character', () =>{
+        const formattedName = formatter.formatFieldName('elis')
+        expect(formattedName).toStrictEqual('Elis')
+    })
+
+    test('Replaces underscore with whitespace', () =>{
+        const formattedName = formatter.formatFieldName('hello_world')
+        expect(formattedName).toStrictEqual('Hello world')
+    })
+})
---
+++ components/products/src/utils/formatter.ts
@@ -0,0 +1,10 @@
+class Formatter{
+    public formatFieldName(fieldName: string): string{
+        const formattedName = (fieldName.charAt(0).toUpperCase() 
+            + fieldName.slice(1)).replaceAll(/_/gi, ' ')
+        
+            return formattedName
+    }
+}
+
+export const formatter = new Formatter()
