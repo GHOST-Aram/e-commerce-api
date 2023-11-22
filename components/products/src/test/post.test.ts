eeb333c05b5ad78ad4a363792b3bf868261e0f6b
ghost-aramic <owinoojala@gmail.com>
2023-11-22 13:08:53 +0300

    chore: Configure test environment
         - Install jest, ts-jest, jest gloabls, supertest and their types
       - Configure jest to work with typescript

--- components/products/src/test/post.test.ts
+++ components/products/src/test/post.test.ts
@@ -2,8 +2,11 @@ import { app } from "../config/config";
 import { routesWrapper } from "../routes/urls";
 import { ProductsController } from "../controller/controller";
 import { ProductsDAL } from "../data-access/data-access";
+import { describe, expect, test } from "@jest/globals"
+
 
 const dal = new ProductsDAL()
 const controller = new ProductsController(dal)
 
 app.use('/products', routesWrapper(controller))
+
