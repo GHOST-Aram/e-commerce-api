ba9692ea7f4b7962530bf513140f0f5bb7f6d53e
ghost-aramic <owinoojala@gmail.com>
2023-11-23 18:03:09 +0300

    refactor : Rewrite Formatter.formatFieldName
              - Replace  '+' operator with String.concat

--- components/products/src/utils/formatter.ts
+++ components/products/src/utils/formatter.ts
@@ -26,12 +26,16 @@ class Formatter{
 
         return priceRange
     }
+
     public formatFieldName(fieldName: string): string{
-        const formattedName = (fieldName.charAt(0).toUpperCase() 
-            + fieldName.slice(1)).replaceAll(/_/gi, ' ')
+        const formattedName = fieldName.charAt(0)
+            .toUpperCase() 
+            .concat(fieldName.slice(1))
+            .replaceAll(/_/gi, ' ')
         
-            return formattedName
+        return formattedName
     }
+
     public isValidNameFormat = (name: string) =>{
         return /^[a-zA-Z\s]{2,100}$/.test(name)
     }
