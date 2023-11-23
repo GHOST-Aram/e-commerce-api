0d65456b95bb278185edb607b75588ab6facca17
ghost-aramic <owinoojala@gmail.com>
2023-11-23 17:58:02 +0300

    refactor: Separate raw data declaration from test file
             - Create a separate mock file for  raw data
             - Import the raw data variables into post.test
             - Use the raw data as user data to create new Product docs

---
+++ components/products/src/test/mocks/raw-document.ts
@@ -0,0 +1,39 @@
+export const productData = {
+    name: "Samsung galaxy A10S",
+    image_url: "https://images/phones/galaxyA10s",
+    description: 'Best smartphone in town. Scholing '+
+        'is hard and sometin sa dahskdjas fkasijj f asjfaso'+
+        ' asjfosj sjafjoasjjf asjoas fajoasj',
+    brand: 'galaxy',
+    manufacturer: 'Samsung',
+    model: 'a10s',
+    category: 'smartphone',
+    selling_price: 10000,
+    marked_price: 10299,
+    available_units: 28, 
+    specifications:[
+        'black', '6 inch', 
+        'display', '3g net',
+            'bluetooth'
+    ],  
+}
+
+export const badData = {
+    name: 90,
+    image_url: 'path',
+    description: 'Best smartphone in town. Scholing '+
+        'is hard and sometin sa dahskdjas fkasijj f '+
+        'asjfaso asjfosj sjafjoasjjf asjoas fajoasj',
+    brand: '',
+    manufacturer: 'Samsung',
+    model: 'a10s',
+    category: 'smartphone',
+    selling_price: 10000,
+    marked_price: 10299,
+    availabile_units: 28, 
+    specifications: [
+        'black', '6 inch', 
+        'display', '3g net',
+            'bluetooth'
+    ],
+}
