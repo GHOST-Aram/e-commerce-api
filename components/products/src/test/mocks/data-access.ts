4266e4ea4a75629fa29a76071105d561b0fcf2cb
ghost-aramic <owinoojala@gmail.com>
2023-11-23 07:10:11 +0300

    feat: Implement route handler
         - Implement GET request handler for all products and single product
           with id
         - Create function in the mock class - findAllProducts &
           findProductById
         - Implement fetching by pagination

--- components/products/src/test/mocks/data-access.ts
+++ components/products/src/test/mocks/data-access.ts
@@ -1,10 +1,41 @@
 import { jest } from "@jest/globals";
-import { IProduct, Product } from "../../data-access/model";
+import { HydratedProductDoc, IProduct, Product } from "../../data-access/model";
 
 export class ProductsDAL {
     createNewProduct = jest.fn(async(productData : IProduct) =>{
-        const product = new Product()
+        const product = new Product(productData)
 
         return product.id
+    })
+
+    findProductById = jest.fn(
+        async(productId: string): Promise<HydratedProductDoc|null> =>{
+            if(productId === '64c9e4f2df7cc072af2ac9e4'){
+                return new Product({
+                    name: 'New Product',
+                    selling_price: 600,
+                    available_units: 18
+                })
+            } else {
+                return null
+            }
+                
+    })
+
+    findProducts = jest.fn(async(skipDocs:  number, limit: number
+        ): Promise<HydratedProductDoc[]> =>{
+        
+            let products:HydratedProductDoc[] = []
+            const product = new Product({
+                name: 'Product Name'
+            })
+
+            let count = 0
+            while(count < limit){
+                products.push(product)
+                count++
+            }
+            
+            return products
     })
 }
