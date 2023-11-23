fe70ccf21c2933d9629ead05c3e26a6007da5054
ghost-aramic <owinoojala@gmail.com>
2023-11-23 10:07:15 +0300

    feat: Implement route handler
          - Implement get request handler for getting product by Manufacturer          name
          - Create getProductsByMAnufacturerName method in controller class
          - Create findProductsByManufacturerName method in data-access class
          - Mock findProductsNameByManufacturerName
          - Implement route handle and data access methods with pagination

--- components/products/src/routes/urls.ts
+++ components/products/src/routes/urls.ts
@@ -7,8 +7,13 @@ const router = Router()
 const routesWrapper = (controller: ProductsController) =>{
     router.post('/',validator.productValidators, controller.AddNewProduct)
     router.get('/', controller.getProducts)
-    router.get('/brands/:brandName', controller.getProductsByBrandName)
     router.get('/:id', controller.getOneProduct)
+    router.get('/brands/:brandName', 
+        controller.getProductsByBrandName
+    )
+    router.get('/manufacturer/:manufacturerName', 
+        controller.getProductsByManufacturerName
+    )
 
     return router
 
