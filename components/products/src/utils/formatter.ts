34733d95782426fdaec8e6177f8785b502fcb501
ghost-aramic <owinoojala@gmail.com>
2023-11-23 13:13:34 +0300

    feat: Create a price range formatter method in Formatter class

--- components/products/src/utils/formatter.ts
+++ components/products/src/utils/formatter.ts
@@ -1,4 +1,31 @@
+export interface PriceRange{
+    start: number | null
+    end: number | null
+}
 class Formatter{
+    public extractPriceRange = (rangeString: string): PriceRange =>{
+
+        let priceRange: PriceRange = {
+            start: null,
+            end: null
+        }
+        const stringArr = rangeString.trim().split('-')
+        
+        const start = Number(stringArr[0])
+        const end = Number(stringArr[1])
+
+        if(typeof start === 'number' && typeof end === 'number'){
+            if(start < end){
+                priceRange.start = start 
+                priceRange.end = end
+            } else if( end < start){
+                priceRange.start = end
+                priceRange.end = start
+            }
+        }
+
+        return priceRange
+    }
     public formatFieldName(fieldName: string): string{
         const formattedName = (fieldName.charAt(0).toUpperCase() 
             + fieldName.slice(1)).replaceAll(/_/gi, ' ')
