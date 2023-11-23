0fc04a100e5211725f6370ed0da1ca88d8467108
ghost-aramic <owinoojala@gmail.com>
2023-11-23 04:50:35 +0300

    chore: Write tests for GET request
          - Test successfull and error responses
          - Test pagination

---
+++ components/products/src/test/get.test.ts
@@ -0,0 +1,71 @@
+import { app } from "../config/config";
+import { routesWrapper } from "../routes/urls";
+import { ProductsController } from "../controller/controller";
+import { ProductsDAL } from "./mocks/data-access";
+import { describe, expect, test } from "@jest/globals"
+import request from "supertest"
+
+
+const dal = new ProductsDAL()
+const controller = new ProductsController(dal)
+
+app.use('/products', routesWrapper(controller))
+
+describe('GET PRODUCTS', () => { 
+    test('Respond with JSON content-type', async() =>{
+        const response = await request(app).get('/products')
+    
+        expect(response.headers['content-type']).toMatch(/json/)
+    })
+
+    test('Responds with an array of all products', async()=>{
+        const response = await request(app).get('/products')
+
+        expect(Array.isArray(response.body.products)).toBeTruthy()
+    })
+
+    test('Responds with 200 status if products found', async() =>{
+        const response = await request(app).get('/products')
+
+        expect(response.status).toEqual(200)
+    })
+
+    test('Responds with single product given ID', async() =>{
+        const response = await request(app).get(
+            '/products/64c9e4f2df7cc072af2ac9e4'
+        )
+        const product = response.body.product
+        expect(product).toHaveProperty('id')
+        expect(product).toHaveProperty('_id')
+        expect(product).toHaveProperty('selling_price')
+        expect(product).toHaveProperty('available_units')
+    })
+
+    test('Responds with 400 Error for invalid product IDs', async() =>{
+        const response = await request(app).get(
+            '/products/64c9e4f2df7ww072rb2vz9x4'
+        )
+
+        expect(response.status).toEqual(400)
+        expect(response.body.error.message).toMatch(/invalid product id/ig)
+    })
+
+    test('Responds with 404 Error for non-existing product IDs', async() =>{
+        const response = await request(app).get(
+            '/products/64c9e4f2df7cc072af2ac9d4'
+        )
+
+        expect(response.status).toEqual(404)
+        expect(response.body.error.message).toMatch(/not found/ig)
+    })
+    
+    test('Responds with paginated data', async() =>{
+        const response = await request(app).get(
+            '/products?page=1&limit=4'
+        )
+        const products = response.body.products
+        expect(products).toHaveProperty('length')
+        expect(products.length).toBeGreaterThan(0)
+        expect(products.length).toBeLessThanOrEqual(4)
+    })
+ })
