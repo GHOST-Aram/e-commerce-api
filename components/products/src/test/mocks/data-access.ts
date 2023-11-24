c681e290e4c533fb09f569276bedb0ee8600dac5
ghost-aramic <owinoojala@gmail.com>
2023-11-24 14:45:26 +0300

    refactor: Encapsulate redundant code
             - Encapsulate the while loop for creating fake products
               array in data-access mock class

--- components/products/src/test/mocks/data-access.ts
+++ components/products/src/test/mocks/data-access.ts
@@ -22,20 +22,28 @@ export class ProductsDAL {
             let products: HydratedProductDoc[] = []
 
             if(categoryName === 'phones'){
-                let productCount = 0
-                while(productCount < paginator.limit){
-                    products.push(new Product({
-                        name: 'Product 10',
-                        brand: 'Samsung',
-                        manufacturer: 'Samsung',
-                        category: 'phones'
-                    }))
-
-                    productCount ++
-                }
+                products = this.createFakeProductsArray(
+                    paginator.limit)
             }
             return products
         })
+
+    private createFakeProductsArray = (
+        paginationLimit: number): HydratedProductDoc[] =>{
+            let products: HydratedProductDoc[] = []
+            let productCount = 0
+
+            while(productCount < paginationLimit){
+                products.push(new Product({
+                    name: 'Product 10',
+                    brand: 'Samsung',
+                    manufacturer: 'Samsung',
+                    category: 'phones'
+                }))
+                productCount ++
+            }
+            return products
+        }
 
     public findProductsByBrand =jest.fn(async (
         brandName: string, 
@@ -43,72 +51,51 @@ export class ProductsDAL {
         ): Promise<HydratedProductDoc[]> => {
             let products: HydratedProductDoc[] = []
 
-            if(brandName === 'samsung'){
-                let productCount = 0
-                while(productCount < paginator.limit){
-                    products.push(new Product({
-                        name: 'Product 10',
-                        brand: 'Samsung',
-                    }))
+            if(brandName === 'samsung')
+                products = this.createFakeProductsArray(
+                    paginator.limit)
 
-                    productCount ++
-                }
-            }
             return products
         })
 
     public findProductById = jest.fn(
         async(productId: string
         ): Promise<HydratedProductDoc|null> =>{
-                if(productId === '64c9e4f2df7cc072af2ac9e4'){
-                    return new Product({
-                        name: 'New Product',
-                        selling_price: 600,
-                        available_units: 18
-                    })
-                } else {
-                    return null
-                }      
-            }
-        )
+            if(productId === '64c9e4f2df7cc072af2ac9e4')
+                return new Product({
+                    name: 'New Product',
+                    selling_price: 600,
+                    available_units: 18
+                })
+            
+            return null         
+        })
 
     public findProductByIdAndDelete = jest.fn(async(
         productId: string
-    ): Promise<HydratedProductDoc | null> =>{
-        if(productId === '64c9e4f2df7cc072af2ac9e4'){
-            const product = new Product({
-                name: 'Deleted product'
-            })
+        ): Promise<HydratedProductDoc | null> =>{
+            if(productId === '64c9e4f2df7cc072af2ac9e4'){
+                const product = new Product({
+                    name: 'Deleted product'
+                })
 
-            return product
-        }
+                return product
+            }
 
-        return null
-    })
+            return null
+        })
 
     public findProductByIdAndUpdate = jest.fn(
         async(productId: string, updateData: IProduct) =>{
             if(productId === '64c9e4f2df7cc072af2ac9e4')
                 return productId
-        return undefined
-    })
-
-    public findProducts = jest.fn(
-        async( {skipDocs, limit}:Paginator
-        ): Promise<HydratedProductDoc[]> =>{
-        
-            let products:HydratedProductDoc[] = []
-            const product = new Product({
-                name: 'Product Name'
-            })
-
-            let count = 0
-            while(count < limit){
-                products.push(product)
-                count++
-            }
             
-            return products
+            return undefined
+        })
+
+    public findProducts = jest.fn(async( paginator:Paginator
+        ): Promise<HydratedProductDoc[]> =>{
+            return this.createFakeProductsArray(paginator.limit)
         })
 
     public findProductsBymanufacturer = jest.fn(async (
@@ -117,18 +104,10 @@ export class ProductsDAL {
         ): Promise<HydratedProductDoc[]> => {
             let products: HydratedProductDoc[] = []
 
-            if(manufacturerName === 'samsung'){
-                let productCount = 0
-                while(productCount < paginator.limit){
-                    products.push(new Product({
-                        name: 'Product 10',
-                        brand: 'Samsung',
-                        manufacturer: 'Samsung'
-                    }))
+            if(manufacturerName === 'samsung')
+                products = this.createFakeProductsArray(
+                    paginator.limit)
 
-                    productCount ++
-                }
-            }
             return products
         })
 
@@ -138,18 +117,10 @@ export class ProductsDAL {
         ): Promise<HydratedProductDoc[]> => {
             let products: HydratedProductDoc[] = []
 
-            if(modelName === 'A10S'){
-                let productCount = 0
-                while(productCount < paginator.limit){
-                    products.push(new Product({
-                        name: 'Product 10',
-                        brand: 'Samsung',
-                        model: 'A10S'
-                    }))
-
-                    productCount ++
-                }
-            }
+            if(modelName === 'A10S')
+                products = this.createFakeProductsArray(
+                    paginator.limit)
+            
             return products
         })
 
@@ -159,21 +130,10 @@ export class ProductsDAL {
         ): Promise<HydratedProductDoc[]> =>{
             let products: HydratedProductDoc[] = []
 
-            if(priceRange.start ===200 && priceRange.end ===800){
-
-                let productCount = 0
-                while(productCount < paginator.limit){
-                    products.push(new Product({
-                        name: 'Product 10',
-                        brand: 'Samsung',
-                        selling_price: 600
-                    }))
-
-                    productCount ++
-                }
-            }
+            if(priceRange.start ===200 && priceRange.end ===800)
+                products = this.createFakeProductsArray(
+                    paginator.limit)
 
             return products
         })
-
 }
