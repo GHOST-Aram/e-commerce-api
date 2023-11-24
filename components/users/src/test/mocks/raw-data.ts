92b31b42bcf4d18f5f4c6b6454b03479015d2836
ghost-aramic <owinoojala@gmail.com>
2023-11-24 18:22:22 +0300

    chore: Write tests for Users POST route

---
+++ components/users/src/test/mocks/raw-data.ts
@@ -0,0 +1,21 @@
+import { IUser } from '../../data-access/model'
+
+export const validUserData: IUser = {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@gmail.com',
+    password: 'password'
+}
+
+export const userWithExistingEmail: IUser = {
+    first_name: 'Existing',
+    last_name: 'User',
+    email: 'existingEmail@gmail.com',
+    password: 'password'
+}
+
+export const invalidUserData = {
+    name: 'John doe',
+    email: 'johndoe@gmail.com',
+    password: 'password'
+}
--- components/users/src/test/post.test.ts
+++ components/users/src/test/post.test.ts
@@ -1,2 +1,45 @@
 import { describe, test, expect } from '@jest/globals'
+import request from 'supertest'
 import {app} from './test.config'
+import * as data from './mocks/raw-data'
+
+describe('POST users', () =>{
+    test('Responds with status 409 if user exists with same email', 
+    async() =>{
+        const response = await request(app).post('/users')
+            .send(data.userWithExistingEmail)
+
+        expect(response.status).toEqual(409)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/exists/)
+    })
+
+    test('Responds with status 400 and errors array '+
+        'for invalid input', 
+    async() =>{
+        const response = await request(app).post('/users')
+            .send(data.invalidUserData)
+
+        expect(response.status).toEqual(400)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/invalid/i)
+        expect(response.body).toHaveProperty('errors')
+        expect(Array.isArray(response.body.errors)).toBeTruthy()
+        expect(response.body.errors.length).toBeGreaterThan(0)
+
+    })
+
+    test('Responds with status 201 if successfuly created', 
+    async() =>{
+        const response = await request(app).post('/users')
+            .send(data.validUserData)
+
+        expect(response.status).toEqual(201)
+        expect(response.body.message).toMatch(/created/)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.header.location).toMatch(
+            /\/users\/^[a-zA-Z0-9]{24}$/)
+    })  
+
+
+})
