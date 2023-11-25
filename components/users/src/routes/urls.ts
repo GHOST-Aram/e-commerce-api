722cae4a2c26beae91c61484cc63a32cd4f0ae5d
ghost-aramic <owinoojala@gmail.com>
2023-11-25 09:12:11 +0300

    refactor: Import dependencies with a wild card
             - Import input all validation chains in routes file
               as middlewear

--- components/users/src/routes/urls.ts
+++ components/users/src/routes/urls.ts
@@ -1,20 +1,23 @@
 import { UsersController } from "../controller/controller";
 import { Router } from "express";
-import { patchValidators, userValidators } from "../utils/middlewears";
+import * as middlewear from "../utils/middlewears";
 
 const router = Router()
 
 export const routesWrapper = (controller: UsersController) =>{
-    router.post('/', userValidators ,controller.AddNewUser)
+    router.post('/', middlewear.userValidators ,
+        controller.AddNewUser)
 
     router.get('/', controller.getMultipleUsers)
     router.get('/:id', controller.getOneUser)
 
     router.put('/', controller.updateAllUsers)
-    router.put('/:id', userValidators, controller.updateOneUser)
+    router.put('/:id', middlewear.userValidators, 
+        controller.updateOneUser)
     
     router.patch('/', controller.modifyAllUsers)
-    router.patch('/:id', patchValidators, controller.modifyOneUser)
+    router.patch('/:id', middlewear.patchValidators, 
+        controller.modifyOneUser)
 
     router.delete('/', controller.removeAllUsers)
     router.delete('/:id', controller.removeOneUser)
