629b2823c102bf0482ff8c65e1ae5a217ba45742
ghost-aramic <owinoojala@gmail.com>
2023-11-26 15:02:17 +0300

    chore: Write tests for carts PUT routes

---
+++ components/carts/src/test/mocks/raw-data.ts
@@ -0,0 +1,13 @@
+export const cartData = {
+    customer: '64c9e4f2df7cc072af2ac9e4',
+    items: ['64c9e4f2df7cc072af2ac9e6']
+}
+
+export const invalidInput = {
+    customer: '64c9e4f2df7',
+}
+
+export const existingCart = {
+    customer: '64c9e4f2df7cc072af2ac9e5',
+    items: ['64c9e4f2df7cc072af2ac9e6']
+}
--- components/carts/src/test/post.test.ts
+++ components/carts/src/test/post.test.ts
@@ -1,5 +1,54 @@
 import { app } from "../config/config";
 import { expect, test, describe } from "@jest/globals"
- test('test1', () =>{
-    expect(1).toBe(1)
- })
+import request from "supertest"
+import * as data from "./mocks/raw-data";
+describe('Cart POST routes', () =>{
+
+	test('Rejects creating cart with client custom IDs (status 405)', 
+	async() =>{
+		const response = await request(app).post(
+			'/carts/64c9e4f2df7cc072af2ac9e4')
+			.send(data.cartData)
+
+		expect(response.status).toEqual(405)
+		expect(response.body.message).toMatch(/not allowed/i)
+		expect(response.headers['content-type']).toMatch(/json/)
+	})
+	
+	test('Responds with conflict if a cart already exists with'+
+		' incoming customer ID (status 409)',
+	async() => {
+		const response = await request(app).post('/carts')
+			.send(data.existingCart)
+
+		expect(response.status).toEqual(409)
+		expect(response.headers['content-type']).toMatch(/json/)
+		expect(response.body.message).toMatch(/exists/)
+	})
+	
+	test('Responds with validation Errors if input is invalid'+
+		' (status 404)', 
+	async() =>{
+		const response = await request(app).post('/carts')
+			.send(data.invalidInput)
+		
+		expect(response.status).toEqual(400)
+		expect(response.body.message).toMatch(/invalid input/i)
+		expect(response.headers['content-type']).toMatch(/json/)
+		expect(response.body).toHaveProperty('errors')
+		expect(Array.isArray(response.body.errors)).toBeTruthy()
+	})
+
+	test('Responds with location url (status 201) if created'+
+		' successfully',
+	async() => {
+		const response = await request(app).post('/carts')
+			.send(data.cartData)
+
+		expect(response.status).toEqual(201)
+		expect(response.body.message).toMatch(/created/i)
+		expect(response.headers['content-type']).toMatch(/json/)
+		expect(response.header.location).toMatch(
+			/\/carts\/[a-fA-F0-9]{24}/)
+	})
+})
