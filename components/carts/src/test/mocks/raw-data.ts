5e9b4cc4c239dc9670bcbfe9e3e27f1c00ff0575
ghost-aramic <owinoojala@gmail.com>
2023-11-26 20:56:47 +0300

    chore: Update PUT routes tests

--- components/carts/src/test/mocks/raw-data.ts
+++ components/carts/src/test/mocks/raw-data.ts
@@ -11,3 +11,19 @@ export const existingCart = {
     customer: '64c9e4f2df7cc072af2ac9e5',
     items: ['64c9e4f2df7cc072af2ac9e6']
 }
+
+export const validUpdateInput = {
+    items: [
+        '64c9e4f2df7cc072af2ac9e6',
+        '64c9e4f2df7cc072af2ac9e6',
+        '64c9e4f2df7cc072af2ac9e6'
+    ]
+}
+
+export const invalidUpdateInput = {
+    items:[
+        '64c9e4fc072af2ac9e6',
+        '64c9e4f2df7cf2ac9e6',
+        '4f2df7cc072af2ac9e6'
+    ]
+}
--- components/carts/src/test/put.test.ts
+++ components/carts/src/test/put.test.ts
@@ -16,15 +16,57 @@ describe('PUT Carts Routes', () => {
         expect(response.body.message).toMatch(/not allowed/i)
     })
 
-    test('Rejects full updates on carts with status 405:'+
-        ' Method not Allowed', 
+    test('Responds with validation errors if input array contains'+
+        ' invalid id: (status 400)', 
     async() =>{
         const response = await request(app).put(
             '/carts/64c9e4f2df7cc072af2ac9e5')
-            .send(data.cartData)
+            .send(data.invalidUpdateInput)
 
-        expect(response.status).toEqual(405)
-        expect(response.body.message).toMatch(/not all/i)
+        expect(response.status).toEqual(400)
+        expect(response.body.message).toMatch(/invalid/i)
         expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('errors')
+        expect(Array.isArray(response.body.errors)).toBeTruthy()
+    })
+
+    test('Responds with status 400 if customer id in request'+
+        ' params is invalid',  
+    async() =>{
+        const response = await request(app).put(
+            '/carts/64c9e4f2dc9e5')
+            .send(data.invalidUpdateInput)
+
+        expect(response.status).toEqual(400)
+        expect(response.body.message).toMatch(/invalid/i)
+        expect(response.headers['content-type']).toMatch(/json/)
+    })
+
+    test('Responds with status 200 and location url if update is '+
+        'successful', 
+    async() =>{
+        const  response = await request(app).put(
+            '/carts/64c9e4f2df7cc072af2ac9e5')
+            .send(data.validUpdateInput)
+
+        expect(response.status).toEqual(200)
+        expect(response.header.location).toMatch(
+            /^\/carts\/64c9e4f2df7cc072af2ac9e5$/)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/updated/i)    
+    })
+
+    test('Responds with status 201 (Created) and resource'+
+        ' location if new cart was created',
+    async() =>{
+        const response = await request(app).put(
+            '/carts/64c9e4f8df7cc072af2ac9e5')
+            .send(data.validUpdateInput)
+
+        expect(response.status).toEqual(201)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.header.location).toMatch(
+            /^\/carts\/64c9e4f8df7cc072af2ac9e5$/)
+        expect(response.body.message).toMatch(/created/i)
     })
 })
