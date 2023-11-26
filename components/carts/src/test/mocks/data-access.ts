13764aeae3ca59f3d32f2e5c3d60a7c339e32f6b
ghost-aramic <owinoojala@gmail.com>
2023-11-26 17:48:48 +0300

    chore: Write tests for GET carts routes

--- components/carts/src/test/mocks/data-access.ts
+++ components/carts/src/test/mocks/data-access.ts
@@ -1,20 +1,38 @@
 import { jest } from "@jest/globals"
 import { Cart, HydratedCartDoc, ICart } from "../../data-access/model"
 import { cartData } from "./raw-data"
+import { Paginator } from "../../controller/controller"
 
 export class DataAccess{
-    public createNewCart = jest.fn(
+    public createNew = jest.fn(
         async(data: ICart): Promise<HydratedCartDoc> =>{
             return new Cart(data)
     })
 
-    public findCartByCustomerId = jest.fn(async(
+    public findByCustomerId = jest.fn(async(
         customerId: string): Promise<HydratedCartDoc | null> =>{
             if(customerId === '64c9e4f2df7cc072af2ac9e5'){
                 return new Cart(cartData)
             }
             return null
     })
+
+    public findWithPagination = jest.fn(
+        async(paginator: Paginator) =>{
+            return this.createFakeCartsArray(paginator.limit)
+    })
+
+    private createFakeCartsArray = (length: number): HydratedCartDoc[] =>{
+        const carts: HydratedCartDoc[] = []
+
+        let count = 0
+        while(count < length){
+            carts.push(new Cart(cartData))
+            count++
+        }
+
+        return carts
+    }
 }
 
 export const dataAccess = new DataAccess()
