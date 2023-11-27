9828de00aea43e7df946a52d9e87257b9a649151
ghost-aramic <owinoojala@gmail.com>
2023-11-27 22:50:29 +0300

    chore: Write tests for DELETE orders routes

---
+++ components/orders/src/test/delete.test.ts
@@ -0,0 +1,51 @@
+import { app } from "./lib/test.config";
+import { expect, test, describe } from "@jest/globals"
+import * as data from "./mocks/raw-data"
+import request from "supertest"
+
+describe('DELETE Orders routes', () =>{
+    test('Rejects delete-all requests with status 405 '+
+        '(Method not allowed)', 
+    async() =>{
+        const response = await request(app).delete('/orders')
+
+        expect(response.status).toEqual(405)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/not allowed/i)
+    })
+
+    test('Rejects requests with invalid order Ids (400 Bad request)', 
+    async() =>{
+        const response = await request(app).delete(
+            '/orders/4f2df7cc072af2ac9e8')
+
+        expect(response.status).toEqual(400)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/Invalid/i)
+    })
+
+    test('Responds with status 404 (Not Found) if target'+
+        ' does not exist', 
+    async() =>{
+        const response = await request(app).delete(
+            '/orders/87c1e4f2df7cc972af2ac9e8')
+
+        expect(response.status).toEqual(404)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body.message).toMatch(/not found/i)
+    })
+
+    test('Responds with status 200 and deletedId if request '+
+        'is successful', 
+    async() =>{
+        const response = await request(app).delete(
+            '/orders/64c9e4f2df7cc072af2ac9e8')
+        
+        expect(response.status).toEqual(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('id')
+        expect(response.body.message).toMatch(/deleted/i)
+        expect(response.body.id).toMatch(/^[a-fA-F0-9]{24}$/)       
+    })  
+
+})
