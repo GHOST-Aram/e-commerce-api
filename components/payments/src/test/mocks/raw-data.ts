6d266d72762d217e1b0651e9e8fab4cc42987822
ghost-aramic <owinoojala@gmail.com>
2023-11-28 10:14:07 +0300

    chore: Write test for POST payments routes

---
+++ components/payments/src/test/mocks/raw-data.ts
@@ -0,0 +1,17 @@
+export const paymentInput = {
+    orderId: '64c9e4f2df7cc072af2ac9e8',
+    processor: 'M-Pesa',
+    amount: 4000,
+    receipt_number: 'PTR4K5HD76',
+    transaction_date: 20231128102117,
+    account_number: 254796699806,
+    currency: 'Ksh'
+}
+
+export const invalidInput = {
+    processor: 'M-Pesa',
+    amount: 'Ksh 4000',
+    receipt_number: '48972',
+    transaction_date: new Date(),
+    account_number: '0796699806',
+}
