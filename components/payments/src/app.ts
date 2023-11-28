df7edbd1ef67958bd1b438cde6c363dd57264040
ghost-aramic <owinoojala@gmail.com>
2023-11-28 07:19:12 +0300

    chore: Generate project starter files for the Payments components

---
+++ components/payments/src/app.ts
@@ -0,0 +1,8 @@
+import { Controller } from "./controller/controller";
+import { dataAccess } from "./data-access/data-access";
+import { routesWrapper } from "./routes/urls";
+import { app } from "./config/config";
+
+const controller = new Controller(dataAccess)
+
+app.use('/app', routesWrapper(controller))
