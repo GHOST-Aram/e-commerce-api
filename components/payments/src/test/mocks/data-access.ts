f460d693a80ec38c7339e73f23e52568e51f788e
ghost-aramic <owinoojala@gmail.com>
2023-11-28 11:12:56 +0300

    feat: Implement Payments POST routes
         - Validate input
         - Reject requests to create payment document with client
           defined Ids
         - Respond with status 201 and resource url in header.location
           if payment document is created successfully

--- components/payments/src/test/mocks/data-access.ts
+++ components/payments/src/test/mocks/data-access.ts
@@ -1,5 +1,12 @@
-export class DataAccess{
+import { jest } from "@jest/globals"
+import { HydratedPaymentDoc, Payment } from "../../data-access/model"
+import { paymentInput } from "./raw-data"
 
+export class DataAccess{
+    public createNewPayment = jest.fn(
+        async(): Promise<HydratedPaymentDoc> =>{
+            return new Payment(paymentInput)
+    })
 }
 
 export const dataAccess = new DataAccess()
---
+++ components/payments/src/utils/middlewear.ts
@@ -0,0 +1,25 @@
+import { validator } from "./validator"
+const required = 'Field is required'
+
+export const postValidators = [
+    validator.validateObjectId('orderId').notEmpty()
+        .withMessage(`orderId ${required}`),
+    
+    validator.validateName('processor').notEmpty()
+        .withMessage(`Processor ${required}`),
+
+    validator.validateAlphanumeric('receipt_number').notEmpty()
+        .withMessage(`Receipt number ${required}`),
+
+    validator.validateNumber('amount').notEmpty().withMessage(
+        `Amount ${`${required}`}`),
+
+    validator.validateName('currency').notEmpty().withMessage(
+        `Currency ${required}`),
+
+    validator.validateNumber('transaction_date').notEmpty()
+        .withMessage(`Transaction Date ${required}`),
+
+    validator.validateNumber('account_number').notEmpty()
+        .withMessage(`Account Number ${required}`)
+]
