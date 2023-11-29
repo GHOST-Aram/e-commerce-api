b18dd9ff7ca4a2c2b90f9310027d7453ce179037
ghost-aramic <owinoojala@gmail.com>
2023-11-29 10:40:31 +0300

    refactor: Rename methods
              - Rename all testing methods in Expectations class

--- components/users/src/test/delete.test.ts
+++ components/users/src/test/delete.test.ts
@@ -1,20 +1,19 @@
 import { app } from "./lib/test.config";
 import { describe, test, expect } from "@jest/globals";
 import request from "supertest"
-import { expectations } from "./lib/response-expectations";
+import { expectations as assert} from "./lib/response-expectations";
 
 describe('DELETE users route', () =>{
     test('Does not allow batch delete request on users (405 Error)', 
     async() =>{
         const response = await request(app).delete('/users')
-        expectations.expectMethodNotAllowedResponse(response)
+        assert.respondsWithMethodNotAllowed(response)
     })
 
     test('Rejects invalid user IDs with status code 400',async () => {
         const response = await request(app).delete(
             '/users/invalidId')
-        expectations.expectBadRequestResponse(response)
-       
+        assert.respondsWithBadRequest(response)  
     })
 
     test('Responds with 404 error if user with requested ID does not'+
@@ -22,16 +21,14 @@ describe('DELETE users route', () =>{
     async() =>{
         const response = await request(app).delete(
             '/users/64c9e4f2df7cc072af2ac8a4')
-       expectations.expectNotFoundResponse(response)
+       assert.respondsWithNotFoundError(response)
     })
 
     test('Responds with 200 if delete request is success', 
     async() =>{
         const response = await request(app).delete(
             '/users/64c9e4f2df7cc072af2ac9e4')
-        
-        expectations.expectSuccessfulDeletionResponse(response)
-        expect(response.body).toHaveProperty('user')
-        expect(response.body.user).toHaveProperty('_id')
+        assert.respondsWithSuccess(response)
+        assert.respondsWithDeletedResource(response)
     })
 })
