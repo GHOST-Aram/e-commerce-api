9ccf89cb498c93e232b5996d14234aac9cb15527
ghost-aramic <owinoojala@gmail.com>
2023-11-29 12:41:00 +0300

    chore: Format controllers class
          - Alter vertical arrangement of methods in the class

--- components/users/src/controller/controller.ts
+++ components/users/src/controller/controller.ts
@@ -32,23 +32,9 @@ export class UsersController{
             }
     }
 
-    public handleValidationErrors = (
-        req: Request, res: Response, next: NextFunction) =>{
-            const errors = validationResult(req)
-
-            if(!errors.isEmpty()){
-                res.status(400).json({
-                    message: 'Invalid input',
-                    errors: errors.array()
-                })
-            } else{
-                next()
-            }
-    }
-
     private respondWithConflict = (res: Response) =>{
         res.status(409).json(
-            {message: 'User exists with same email'})
+            { message: 'User exists with same email' })
     }
 
     private respondWithCreatedResource = (
@@ -61,31 +47,27 @@ export class UsersController{
             })
     }
 
-    public removeOneUser = async(
-        req: Request, res: Response,next: NextFunction) =>{
+    public getOneUser = async(
+        req: Request, res: Response, next: NextFunction
+        ) =>{
             const userId = req.params.id
 
             try {
-                const deletedUser = await this.dal.findUserByIdAndDelete(
-                    userId)
-                if(deletedUser)
-                    this.respondWithDeletedResource(
-                    deletedUser.id, res)
+                const user = await this.dal.findUserById(userId)
                 
-                this.respondWith404Error(res)
+                if(user === null)
+                    this.respondWithNotFound(res)
+                else     
+                    this.respondWithFoundResource(user, res)
             } catch (error) {
                 next(error)
             }
     }
 
-    private respondWithDeletedResource = (
-        id: string, res: Response) =>{
-            res.status(200).json({
-                message: 'deleted',
-                id: id
-            })
+    private respondWithNotFound = (res: Response) =>{
+        res.status(404).json({ message: 'User ID not found'})
     }
-
+    
     public getMultipleUsers = async(
         req: Request, res: Response, next: NextFunction
         ) =>{
@@ -100,6 +82,12 @@ export class UsersController{
             }
     }
 
+    private respondWithFoundResource = (
+        resource: HydratedUserDoc[] | HydratedUserDoc, 
+        res: Response) =>{
+            res.status(200).json({ resource: resource  })   
+    }
+
     private paginate = (req: Request): Paginator =>{
         const page = Number(req.query.page)
         const limit = Number(req.query.limit)
@@ -110,34 +98,6 @@ export class UsersController{
         }
 
         return { skipDocs, limit }
-    }
-
-    private respondWithFoundResource = (
-        resource: HydratedUserDoc[] | HydratedUserDoc, 
-        res: Response) =>{
-            res.status(200).json({ resource: resource  })   
-    }
-
-    public getOneUser = async(
-        req: Request, res: Response, next: NextFunction
-        ) =>{
-            const userId = req.params.id
-
-            try {
-                const user = await this.dal.findUserById(userId)
-                
-                if(user === null)
-                    this.respondWith404Error(res)
-                else     
-                    this.respondWithFoundResource(user, res)
-            } catch (error) {
-                next(error)
-            }
-    }
-
-    private handleInvalidId = (id: string, res: Response) =>{
-        if(!isValidObjectId(id))
-            res.status(400).json({ message: 'Invalid user id'})
     }
 
     public modifyOneUser = async(
@@ -154,16 +114,11 @@ export class UsersController{
                     this.respondWithChangedResource(
                         user.id,'Modified', res)
 
-                this.respondWith404Error(res)
+                this.respondWithNotFound(res)
             } catch (error) {
                 next(error)
             } 
 
-    }
-
-
-    public respondWithMethodNotAllowed = (req: Request, res: Response) =>{
-        res.status(405).json({ message: 'Method not allowed'})
     }
 
     public updateOneUser = async(
@@ -194,8 +149,47 @@ export class UsersController{
             res.status(200).json({ message: change})
     }
 
-    private respondWith404Error = (res: Response) =>{
-        res.status(404).json({ message: 'User ID not found'})
+    public removeOneUser = async(
+        req: Request, res: Response,next: NextFunction) =>{
+            const userId = req.params.id
+
+            try {
+                const deletedUser = await this.dal.findUserByIdAndDelete(
+                    userId)
+                if(deletedUser)
+                    this.respondWithDeletedResource(
+                    deletedUser.id, res)
+                
+                this.respondWithNotFound(res)
+            } catch (error) {
+                next(error)
+            }
+    }
+
+    private respondWithDeletedResource = (
+        id: string, res: Response) =>{
+            res.status(200).json({
+                message: 'deleted',
+                id: id
+            })
+    }
+
+    public respondWithMethodNotAllowed = (req: Request, res: Response) =>{
+        res.status(405).json({ message: 'Method not allowed'})
+    }
+
+    public handleValidationErrors = (
+        req: Request, res: Response, next: NextFunction) =>{
+            const errors = validationResult(req)
+
+            if(!errors.isEmpty()){
+                res.status(400).json({
+                    message: 'Invalid input',
+                    errors: errors.array()
+                })
+            } else{
+                next()
+            }
     }
 }
 
