07503eb95e10810fefe350579f59cfbc42255e4b
ghost-aramic <owinoojala@gmail.com>
2023-11-29 12:58:36 +0300

    refactor: Relocate class method
             - Move validation error handling middlewear from
               Controller to Validator class

--- components/users/src/routes/urls.ts
+++ components/users/src/routes/urls.ts
@@ -9,14 +9,14 @@ export const routesWrapper = (controller: UsersController) =>{
     router.post('/:id', controller.respondWithMethodNotAllowed)
     router.post('/', 
         middlewear.userValidators ,
-        controller.handleValidationErrors,
+        validator.handleValidationErrors,
         controller.AddNew
     )
 
     router.get('/', controller.getMany)
     router.get('/:id', 
         validator.validateReferenceId('id'),
-        controller.handleValidationErrors,
+        validator.handleValidationErrors,
         controller.getOne
     )
 
@@ -24,7 +24,7 @@ export const routesWrapper = (controller: UsersController) =>{
     router.put('/:id', 
         validator.validateReferenceId('id'),
         middlewear.userValidators, 
-        controller.handleValidationErrors,
+        validator.handleValidationErrors,
         controller.updateOne
     )
     
@@ -32,14 +32,14 @@ export const routesWrapper = (controller: UsersController) =>{
     router.patch('/:id', 
         validator.validateReferenceId('id'),
         middlewear.patchValidators, 
-        controller.handleValidationErrors,
+        validator.handleValidationErrors,
         controller.modifyOne
     )
 
     router.delete('/', controller.respondWithMethodNotAllowed)
     router.delete('/:id', 
         validator.validateReferenceId('id'),
-        controller.handleValidationErrors,
+        validator.handleValidationErrors,
         controller.removeOne
     )
 
