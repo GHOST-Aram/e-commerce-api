f2cf5ca0e03281c477f1db48a460ce2539bc2415
ghost-aramic <owinoojala@gmail.com>
2023-11-29 10:40:31 +0300

    refactor: Rename methods
              - Rename all testing methods in Expectations class

--- components/users/src/test/get.test.ts
+++ components/users/src/test/get.test.ts
@@ -1,55 +1,40 @@
 import { app } from "./lib/test.config";
-import { describe, test, expect } from "@jest/globals";
+import { describe, test } from "@jest/globals";
 import request from 'supertest'
-import { expectations } from "./lib/response-expectations";
+import { expectations as assert} from "./lib/response-expectations";
 
 describe('GET Users Route', () =>{
 
-    test('Responds with 200 status and users array on success ', 
+    test('Responds with paginated users array: Default lenght 10 ', 
     async() =>{
         const response = await request(app).get('/users')
-        const users = response.body.users
         
-        expectations.expectSuccessfulResponse(response)
-        expectations.expectResponseWithPaginatedUsersArray(
-            response, 10)
-        expect(users[0]).not.toHaveProperty('password')
+        assert.respondsWithSuccess(response)
+        assert.respondsWithPaginatedResource(response, 10)
     })
 
-    test('Responds with paginated data of default limit = 10', 
-    async() =>{
-        const response = await request(app).get('/users')
-        const users = response.body.users
-
-        expectations.expectSuccessfulResponse(response)
-        expectations.expectResponseWithPaginatedUsersArray(
-            response, 10)
-        expect(users[0]).not.toHaveProperty('password')
-    })
-
-    test('Responds with paginated data limited to given limit', 
+    test('Responds with paginated array: Length equals given limit', 
     async() =>{
         const response = await request(app).get(
             '/users?page=1&limit=23')
 
-        expectations.expectSuccessfulResponse(response)
-        expectations.expectResponseWithPaginatedUsersArray(
-            response, 23)
+        assert.respondsWithSuccess(response)
+        assert.respondsWithPaginatedResource(response, 23)
     } )
 
-    test('Responds with status 400 if user ID is invalid',  
+    test('Responds with validationErrors:  If referenceId is Invalid',  
     async() =>{
         const response = await request(app).get(
             '/users/9jdiks9sk0xx34')
-        expectations.expectBadRequestResponse(response)
+        assert.respondsWithBadRequest(response)
+        assert.respondsWithValidationErrorsArray(response)
     })
 
-    test('Responds with status 404 if user with a valid id '+
-        'don\'t exist', 
+    test('Responds with Not Found if requested user does not exist', 
     async() =>{
         const response = await request(app).get(
             '/users/64c9e4f2df7cc072af2ac8a4')
-        expectations.expectNotFoundResponse(response)
+        assert.respondsWithNotFoundError(response)
     })
 
     test('Responds with 200 if user exists with a valid ID', 
@@ -57,7 +42,7 @@ describe('GET Users Route', () =>{
         const response = await request(app).get(
             '/users/64c9e4f2df7cc072af2ac9e4')
         
-        expectations.expectSuccessfulResponse(response)
-        expectations.expectResponseWithUserDocument(response)
+        assert.respondsWithSuccess(response)
+        assert.respondsWithFoundResource(response)
     })
 })
