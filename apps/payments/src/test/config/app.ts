bc9c77009fa44b6b983edee77b9ef540ae2730fb
ghost-aramic <owinoojala@gmail.com>
2023-12-07 17:56:58 +0300

    refactor: Decouple test configs from production configs
            - Create new app and routes configs for tests

--- apps/payments/src/test/config/app.ts
+++ apps/payments/src/test/config/app.ts
@@ -1,7 +1,12 @@
 import { PayController } from "../../controller/controller";
 import { dataAccess } from "../mocks/data-access";
-import { routesWrapper } from "../../routes/urls";
-import { app } from "../../config/config";
+import { routesWrapper } from "./urls";
+import express from 'express'
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 const controller = new PayController(dataAccess)
 
---
+++ apps/payments/src/test/config/urls.ts
@@ -0,0 +1,47 @@
+    import Router from 'express'
+    import { PayController } from '../../controller/controller'
+    import * as middlewear from '../../z-library/validators/middlewear'
+    import { validator } from '../../z-library/validators/validator'
+
+    const router = Router()
+
+    export const routesWrapper = (controller: PayController) =>{
+        router.post('/:paymentId',controller.respondWithMethodNotAllowed)
+        router.post('/', 
+            middlewear.postValidators,
+            validator.handleValidationErrors, 
+            controller.addNew
+        )
+
+        router.get('/',controller.getMany)
+        router.get('/:orderId', 
+            middlewear.referenceIdValidator,
+            validator.handleValidationErrors,
+            controller.getOne
+        )
+
+        router.put('/', controller.respondWithMethodNotAllowed)
+        router.put('/:orderId',
+            middlewear.postValidators,
+            middlewear.referenceIdValidator,
+            validator.handleValidationErrors,
+            controller.updateOne
+        )
+
+        router.patch('/', controller.respondWithMethodNotAllowed)
+        router.patch('/:orderId',
+            middlewear.patchValidators,
+            middlewear.referenceIdValidator,
+            validator.handleValidationErrors,
+            controller.modifyOne
+        )
+
+        router.delete('/', controller.respondWithMethodNotAllowed)
+        router.delete('/:orderId', 
+            middlewear.referenceIdValidator,
+            validator.handleValidationErrors,
+            controller.deleteOne
+        )
+
+        return router
+    }
