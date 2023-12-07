83da3963b116f5602fa905cf05ceb56968fbb0f9
ghost-aramic <owinoojala@gmail.com>
2023-12-07 17:58:17 +0300

    feat: Set up authentication
             - Use passport and passport jwt
             - create authentication class to act as boundary between passport
               and payments

--- apps/payments/src/app.ts
+++ apps/payments/src/app.ts
@@ -1,16 +1,14 @@
 import { PayController } from "./controller/controller";
 import { dataAccess } from "./data-access/data-access";
-import { routesWrapper } from "./routes/urls";
+import { routesWrapper } from "./test/config/urls";
 import { httpErrors } from "./z-library/HTTP/http-errors";
 import { app } from "./config/config";
-import logger from "morgan"
+import { authenticator } from "./z-library/auth/auth";
 
-//Log requests
-app.use(logger('dev'))
 
 const controller = new PayController(dataAccess)
 
-app.use('/payments', routesWrapper(controller))
+app.use('/payments', authenticator.authenticate(), routesWrapper(controller))
 
 //Handle errors -- Unknown path
 app.use(httpErrors.handleUnknownUrls)
