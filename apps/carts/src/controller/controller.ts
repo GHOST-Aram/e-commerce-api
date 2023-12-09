2cd6c9a7d7ce5ad59260ede5cede5f3860a0a69c
ghost-aramic <owinoojala@gmail.com>
2023-12-09 13:06:09 +0300

    refactor: Check authentication within controller
             - Check if requests have user data before processing
             - If request has not user data, respond with unauthorised

--- apps/carts/src/controller/controller.ts
+++ apps/carts/src/controller/controller.ts
@@ -1,147 +1,183 @@
 import { HttpResponse, Paginator } from "../z-library/HTTP/http-response"
 import { Controllable } from "../z-library/bases/controllable"
-import { HydratedCartDoc, Cart } from "../data-access/model"
+import { Cart } from "../data-access/model"
 import { NextFunction, Request, Response } from "express"
 import { DataAccess } from "../data-access/data-access"
 
 export class CartsController extends HttpResponse implements Controllable{
 
-    private dal: DataAccess
+    private dataAccess: DataAccess
 
     constructor(dataAccess: DataAccess){
         super()
-        this.dal = dataAccess
+        this.dataAccess = dataAccess
     }
 
     public addNew = async(req: Request, res: Response, next: NextFunction) =>{
-        const { items } = req.body
         const currentUser:any = req.user
         
-        try {
-            if(currentUser){
+        if(currentUser){
+            const { items } = req.body
+
+            try {
                 const currentUserId = currentUser?._id.toString()
-                const existingCart = await this.dal.findByReferenceId (
+                const existingCart = await this.dataAccess.findByReferenceId (
                     currentUserId)
                     
                 if(existingCart== null){
                 const cartData: Cart = { items, customer:currentUserId}
                 
-                const newCart = await this.dal.createNew(cartData)  
+                const newCart = await this.dataAccess.createNew(cartData)  
                 this.respondWithCreatedResource(newCart.customer, res)
                 } else {
                     this.respondWithConflict(res)
                 }
-            } else {
-                this.respondWithUnauthorised(res)
+            } catch (error) {
+                next(error)
             }
-        } catch (error) {
-            next(error)
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 
     public getOne = async(req: Request, res: Response, next: NextFunction) =>{
 
-        const customerId = req.params.customerId
+        const currentUser:any = req.user
+        
+        if(currentUser){
+            const customerId = currentUser._id.toString()
 
-        try {
-            const cart = await this.dal.findByReferenceId(customerId)
-
-            if(cart)
-                this.respondWithFoundResource(cart, res)
-            else
-                this.respondWithNotFound(res)
-        } catch (error) {
-            next(error)   
+            try {
+                const cart = await this.dataAccess.findByReferenceId(customerId)
+    
+                if(cart)
+                    this.respondWithFoundResource(cart, res)
+                else
+                    this.respondWithNotFound(res)
+            } catch (error) {
+                next(error)   
+            }
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 
     public getMany = async(req: Request, res: Response, next: NextFunction) =>{
+        const currentUser:any = req.user
 
-        const paginator: Paginator = this.paginate(req)
+        if(currentUser){
+            const paginator: Paginator = this.paginate(req)
 
-        try {
-            const carts = await this.dal.findWithPagination(paginator)
-            this.respondWithFoundResource(carts, res)
-        } catch (error) {
-            next(error)
+            try {
+                const carts = await this.dataAccess.findWithPagination(paginator)
+                this.respondWithFoundResource(carts, res)
+            } catch (error) {
+                next(error)
+            }
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 
     public updateOne = async(req: Request, res: Response, next: NextFunction) =>{
+        const currentUser:any = req.user
 
-        const customerId = req.params.customerId
-        const updateDoc: {items: string[]} = req.body
-
-        try {
-            const updatedCart = await this.dal
-                .findByIdAndUpdate(customerId, updateDoc)
-            if(updatedCart){
-                this.respondWithUpdatedResource(updatedCart.customer,res)
-            } else {
-                const newCart = await this.dal.createNew({
-                    customer: customerId,
-                    items: updateDoc.items,
-                })
-
-                this.respondWithCreatedResource(newCart.customer, res)
+        if(currentUser){
+            const customerId = currentUser._id.toString()
+            const updateDoc: {items: string[]} = req.body
+    
+            try {
+                const updatedCart = await this.dataAccess.findByIdAndUpdate(
+                    customerId, updateDoc)
+                if(updatedCart){
+                    this.respondWithUpdatedResource(updatedCart.customer,res)
+                } else {
+                    const newCart = await this.dataAccess.createNew({
+                        customer: customerId,
+                        items: updateDoc.items,
+                    })
+    
+                    this.respondWithCreatedResource(newCart.customer, res)
+                }
+            } catch (error) {
+                next(error)
             }
-        } catch (error) {
-            next(error)
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 
     public addtoCart = async(req: Request, res: Response, next: NextFunction) =>{
+        const currentUser:any = req.user
 
-        const customerId = req.params.customerId
-        const itemId: string = req.body.item
-
-        try {
-            const modifiedCart = await this.dal.findCartAndAddItemId(
-                customerId, itemId)
-                
-            if(modifiedCart){
-                this.respondWithModifiedResource(modifiedCart.customer, res)
-            } else {
-                this.respondWithNotFound(res)
-            }    
-        } catch (error) {
-            next(error)
+        if(currentUser){
+            const customerId = currentUser._id.toString()
+            const itemId: string = req.body.item
+    
+            try {
+                const modifiedCart = await this.dataAccess.findCartAndAddItemId(
+                    customerId, itemId)
+                    
+                if(modifiedCart){
+                    this.respondWithModifiedResource(modifiedCart.customer, res)
+                } else {
+                    this.respondWithNotFound(res)
+                }    
+            } catch (error) {
+                next(error)
+            }
+        } else {
+            this.respondWithUnauthorised(res)
         }
+
     }
 
     public removeFromCart = async(req: Request, res: Response, next: NextFunction
         ) =>{
-        const customerId = req.params.customerId
-        const itemId: string = req.body.item
 
-        try {
-            const modifiedCart = await this.dal.findCartAndRemoveItemId(
-                customerId, itemId
-            )
-            if(modifiedCart){
-                this.respondWithModifiedResource( modifiedCart.customer, res)
-            } else {
-                this.respondWithNotFound(res)
-            }    
-        } catch (error) {
-            next(error)
+        const currentUser:any = req.user
+
+        if(currentUser){
+            const customerId = currentUser._id.toString()
+            const itemId: string = req.body.item
+    
+            try {
+                const modifiedCart = await this.dataAccess.findCartAndRemoveItemId(
+                    customerId, itemId
+                )
+                if(modifiedCart){
+                    this.respondWithModifiedResource( modifiedCart.customer, res)
+                } else {
+                    this.respondWithNotFound(res)
+                }    
+            } catch (error) {
+                next(error)
+            }
+
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 
     public deleteOne = async(req: Request, res: Response, next:NextFunction) =>{
+        const currentUser:any = req.user
 
-        const customerId = req.params.customerId
-
-        try {
-            const deletedCart = await this.dal.findByIdAndDelete(customerId)
-            
-            if(deletedCart){
-                this.respondWithDeletedResource(deletedCart.customer, res)
-            } else {
-                this.respondWithNotFound(res)
+        if(currentUser){
+            const customerId = currentUser._id.toString()
+    
+            try {
+                const deletedCart = await this.dataAccess.findByIdAndDelete(customerId)
+                
+                if(deletedCart){
+                    this.respondWithDeletedResource(deletedCart.customer, res)
+                } else {
+                    this.respondWithNotFound(res)
+                }
+            } catch (error) {
+                next(error)
             }
-        } catch (error) {
-            next(error)
+        } else {
+            this.respondWithUnauthorised(res)
         }
     }
 }
