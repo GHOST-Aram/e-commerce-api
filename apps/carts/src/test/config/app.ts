3c57a69fd16d46edee808f696250818fad9ee3ba
ghost-aramic <owinoojala@gmail.com>
2023-12-13 09:32:58 +0300

    refactor: Delete redundant file
             - Delete the urls file in test configs
             - Use one url file for unit tests and for the main application

--- apps/carts/src/test/config/app.ts
+++ apps/carts/src/test/config/app.ts
@@ -1,7 +1,7 @@
 import { CartsController } from "../../controller/controller";
 import { Cart } from "../../data-access/model";
 import { DataAccess } from "../mocks/data-access";
-import { routesWrapper } from "./urls";
+import { routesWrapper } from "../../routes/urls";
 import express from 'express'
 import { mockAuth } from "../mocks/auth";
 const app = express()
