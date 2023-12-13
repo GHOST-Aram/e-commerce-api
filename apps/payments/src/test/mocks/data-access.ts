52a2213252c5baa563a37b33252393905294c0da
ghost-aramic <owinoojala@gmail.com>
2023-12-13 13:01:54 +0300

    refactor: Rename variables
              - Rename variables in Payments data access class

--- apps/payments/src/test/mocks/data-access.ts
+++ apps/payments/src/test/mocks/data-access.ts
@@ -9,56 +9,57 @@ export class DataAccess{
     constructor(model: PaymentModel){
         this.Model = model
     }
+
     public createNew = jest.fn(
         async(): Promise<HydratedPaymentDoc> =>{
-            return new this.Model(paymentInput)
-    })
+            const mockPayment =  new this.Model(paymentInput)
+            return mockPayment
+        }
+    )
 
     public findByReferenceId = jest.fn(
         async(orderId: string): Promise<HydratedPaymentDoc | null> =>{
             if(orderId === '64c9e4f2df7cc072af2ac9e8'){
-                return new this.Model(paymentInput)
-            } else {
-                return null
-            }
-    })
+                const mockFoundPayment =  new this.Model(paymentInput)
+                return mockFoundPayment
+            } else return null
+        }
+    )
 
     public findWithPagination = jest.fn(
         async(paginator: Paginator
             ): Promise<HydratedPaymentDoc[]> =>{
-                return this.createDocsArray(paginator.limit)
+                const mockPayments =  this.createDocsArray(paginator.limit)
+                return mockPayments
         }    
     )
 
     private createDocsArray = async(length: number
         ): Promise<HydratedPaymentDoc[]> =>{
-            const payments: HydratedPaymentDoc[] = []
+            const mockPayments: HydratedPaymentDoc[] = []
 
             let count = 0
             while(count < length){
-                payments.push(new this.Model(paymentInput))
+                mockPayments.push(new this.Model(paymentInput))
                 count ++
             }
             
-            return payments
+            return mockPayments
     }
 
-    public findByIdAndUpdate = jest.fn(
-        async(orderId: string, updateDoc: Payment
-            ): Promise<HydratedPaymentDoc | null> =>{
-                if(orderId === '64c9e4f2df7cc072af2ac9e8'){
-                    return new this.Model(paymentInput)
-                } else {
-                    return null
-                }
+    public findByIdAndUpdate = jest.fn(async(orderId: string, updateDoc: Payment
+        ): Promise<HydratedPaymentDoc | null> =>{
+            if(orderId === '64c9e4f2df7cc072af2ac9e8'){
+                const mockUpdatedPayment =  new this.Model(paymentInput)
+                return mockUpdatedPayment
+            } else  return null
     })
 
     public findByIdAndDelete = jest.fn(
         async(orderId: string): Promise<HydratedPaymentDoc | null> =>{
             if(orderId === '64c9e4f2df7cc072af2ac9e8'){
-                return new this.Model(paymentInput)
-            } else {
-                return null
-            }
+                const mockDeletedPayment =  new this.Model(paymentInput)
+                return mockDeletedPayment
+            } else return null
     })
 }
