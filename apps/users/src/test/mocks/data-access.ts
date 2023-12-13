5ac0bd540d4a061cdbde7ee55de1503fba40c33e
ghost-aramic <owinoojala@gmail.com>
2023-12-13 15:15:35 +0300

    refactor: Rename variables
             - Rename variables in Users mock data access class

--- apps/users/src/test/mocks/data-access.ts
+++ apps/users/src/test/mocks/data-access.ts
@@ -12,50 +12,61 @@ export class UsersDAL{
     constructor(model: UserModel){
         this.Model= model
     }
-    public createNew = jest.fn(async(
-        userData: User): Promise<HydratedUserDoc> =>{
-        return new this.Model(userData)  
+    public createNew = jest.fn(async(userData: User): Promise<HydratedUserDoc> =>{
+        
+        const mockUser = new this.Model(userData)  
+        return mockUser
     })
 
     public findByReferenceId = jest.fn(async(userID: string
         ): Promise<HydratedUserDoc | null> =>{
-            if(userID === '64c9e4f2df7cc072af2ac9e4'){
-                const user = new this.Model({
+
+            const availableDocumentID = '64c9e4f2df7cc072af2ac9e4'
+
+            if(userID === availableDocumentID){
+                const mockFoundUser = new this.Model({
                     last_name: 'John',
                     first_name: 'Does',
                     email: 'johndoe@gmail.com'
                 })
 
-                return user
+                return mockFoundUser
             }
 
             return null
-    
     })
 
-    public findByEmail = jest.fn(async(email: string
-        ): Promise<HydratedUserDoc | null> =>{
-        if(email === 'existingEmail@gmail.com')
-            return new this.Model({
+    public findByEmail = jest.fn(async(email: string): Promise<HydratedUserDoc | null> =>{
+
+        const existingDocumentEmail = 'existingEmail@gmail.com'
+
+        if(email === existingDocumentEmail){
+
+            const mockDocumentWithExistingEmail =  new this.Model({
                 last_name: 'John',
                 first_name: 'Does',
                 email: 'existingEmail@gmail.com'
-        })
+            })
 
-        return null
+            return mockDocumentWithExistingEmail
+
+        } else return null
     })
     
     public findWithPagination = jest.fn(async(
         pagination: Paginator): Promise<HydratedUserDoc[]> =>{
-            return this.createFakeDocsArray(pagination.limit)
+
+            const mockUsers =  this.createMockUsersArray(pagination.limit)
+            return mockUsers
     })
 
-    private createFakeDocsArray = (limit: number) =>{
-        const users: HydratedUserDoc[] = []
+    private createMockUsersArray = (limit: number) =>{
+
+        const mockUsers: HydratedUserDoc[] = []
 
         let userCount = 0
         while(userCount < limit){
-            users.push(new this.Model({
+            mockUsers.push(new this.Model({
                 first_name: 'John',
                 last_name: 'Doe',
                 email: 'johnDoes@gmail.com'
@@ -64,31 +75,40 @@ export class UsersDAL{
             userCount ++
         }
 
-        return users
+        return mockUsers
     }
    
     public findByIdAndDelete = jest.fn(
         async(userId: string): Promise<HydratedUserDoc | null> =>{
-            if(userId === '64c9e4f2df7cc072af2ac9e4'){
-                return new this.Model({
+
+            const existingDocumentId = '64c9e4f2df7cc072af2ac9e4'
+
+            if(userId === existingDocumentId){
+                const mockDeletedUserDoc =  new this.Model({
                     first_name: 'John', 
                     last_name: 'Doe',
                     email: 'johndoe@gmail.com'
                 })
-            }
 
-            return null
+                return mockDeletedUserDoc
+
+            } else return null
     })
+
     public findByIdAndUpdate = jest.fn(
         async(userId: string): Promise<HydratedUserDoc | null> =>{
-            if(userId === '64c9e4f2df7cc072af2ac9e4'){
-                return new this.Model({
+
+            const existingDocumentId = '64c9e4f2df7cc072af2ac9e4'
+
+            if(userId === existingDocumentId){
+                const mockUpdatedUserDoc =  new this.Model({
                     first_name: 'John', 
                     last_name: 'Doe',
                     email: 'johndoe@gmail.com'
                 })
-            }
 
-            return null
+                return mockUpdatedUserDoc
+
+            } else return null
     })  
 }
