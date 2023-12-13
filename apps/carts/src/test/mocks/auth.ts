749ab9fd0bf7d73d76be27652b83e6f5aab622f7
ghost-aramic <owinoojala@gmail.com>
2023-12-13 09:34:49 +0300

    refactor: Extend mock auth to cover all tests
            - Add admin property to user object in carts mock auth to allow
              for testing of all routes including the ones that require admin level

--- apps/carts/src/test/mocks/auth.ts
+++ apps/carts/src/test/mocks/auth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export const mockAuth = (req: Request, res: Response, next: NextFunction) =>{
     if(req.body.customer){
-        const user = {_id: req.body.customer }
+        const user = {_id: req.body.customer, isAdmin: true }
         req.user = user
         req.isAuthenticated = () => Boolean(req.user)
         next()
