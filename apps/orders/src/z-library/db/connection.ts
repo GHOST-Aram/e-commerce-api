62c8f9f51f3aac16e7b9c09ddd2d8cb4a5aa2745
ghost-aramic <owinoojala@gmail.com>
2023-12-14 11:00:53 +0300

    chore: Handle DB conection errors
           - Handle unhandled promise rejection in z-libray/db/connection
             class constructor

--- apps/orders/src/z-library/db/connection.ts
+++ apps/orders/src/z-library/db/connection.ts
@@ -6,7 +6,21 @@ export class Connection {
 
     constructor(intialDbUri: string){
         this.initialConnection = mongoose.createConnection(intialDbUri)
+
         
+        this.initialConnection.on('error', (error) => {
+            if(error){
+                console.log('Database connection failed')
+            }
+        })
+
+        process.on('unhandledRejection', (reason: any, Promise) =>{
+            console.log('Reason for failure - ', reason.stack)
+        })
+
+        this.initialConnection.on('connected', () => {
+            console.log('Application Connected to Database')
+        })  
     }
 
     public getInitial = (): mongoose.Connection =>{
